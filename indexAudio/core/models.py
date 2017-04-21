150e51cd21050f7bca637b15242016549cf7f5e3
Andre Sazonov <andre.sazonov@gmail.com>
2017-04-21 20:33:55 -0300

    configurando models

--- indexAudio/core/models.py
+++ indexAudio/core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Empresas(models.Model):
+    empresa = models.CharField(max_length=200)
+
+
+class Audios(models.Model):
+    idx = models.IntegerField(default=0)
+    nome = models.CharField(max_length=200)
+    empresa = models.ForeignKey(Empresas, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField('Data Criacao')
